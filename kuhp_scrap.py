0506d422108df5705df239bd04f5c751517d388f
Kevin <khalfanifadillah.com>
2024-05-12 19:10:35 +0700

    Changes to be committed: 	modified:   kuhp_scrap.py

--- kuhp_scrap.py
+++ kuhp_scrap.py
@@ -1,8 +1,13 @@
 import PyPDF2
 import re
+import pandas as pd
+from textblob import TextBlob
+import nltk
+from nltk.corpus import stopwords
 
 text = ""
 
+# Function to extract text from PDF
 def extract_text_from_pdf(pdf_path, exclude_patterns = None):
     text = ""
     if exclude_patterns is None:
@@ -22,6 +27,7 @@ def extract_text_from_pdf(pdf_path, exclude_patterns = None):
             text += page_text
     return text
 
+# Function to classify the text based on pasal
 def classifyPasal(text):
     # Split the text into sections based on occurrences
     sections = re.split(r'pasal \d+', text, flags=re.IGNORECASE)
@@ -34,8 +40,34 @@ def classifyPasal(text):
 
     return sections
 
+# Function to find all typos
+def correctedTypos(text):
+    df = pd.DataFrame({'Peraturan': text})
+    top5 = df.head()
+    df['Peraturan'] = top5['Peraturan'].apply(lambda x: str(TextBlob(x).correct()))
+    return df
+
+# Function to find all common words
+def removeStopWords():
+    df = pd.DataFrame({'Peraturan': text})
+    stop = stopwords.words('indonesia')
+    df['Peraturan'] = df['Peraturan'].apply(lambda x: "".join(x for x in x.split() if x not in stop))
+
+
+
+
+
+
+# Calling the extract text function
 pdf_text = extract_text_from_pdf("C:/Users/khalf/Code/NLP/Undang-Undang Nomor 1 Tahun 20230.pdf", exclude_patterns=["Presiden Republik Indonesia", "SK No 16003 A"])
+
+# Classifying all the text based on pasal
 classified_text = classifyPasal(pdf_text)
-for section in classified_text:
-    print(section)
-    print("------------")
+
+# Correct typos in classified text
+corrected_text_df = correctedTypos(classified_text)
+
+#for index, row in corrected_text_df.iterrows():
+#   print(row['Peraturan'])
+
+print(corrected_text_df.shape)
